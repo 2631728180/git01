854ef9a7cee5865e17a72a920275b54ceb013f12
jyl438 <47443115+2631728180@users.noreply.github.com>
2020-03-10 16:06:01 +0800

    sd

--- 1.js
+++ 1.js
@@ -3,3 +3,11 @@
     console.log(2)
     console.log(3)
 })()
+
+function e() {
+    var timer = null
+    timer = timer != null ? null : timer
+    var timer = setTimeout(function() {
+        console.log(5)
+    }, 5000)
+}
